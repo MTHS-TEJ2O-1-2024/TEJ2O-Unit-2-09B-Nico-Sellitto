bcc3c69a5d526f6ce930591c331dd109a1fe2827
Nico-Sellitto <nico.sellitto@mths.ca>
2024-10-09 11:07:45 -0400

    programmed

--- main.ts
+++ main.ts
@@ -5,4 +5,54 @@
  * This program is rock paper scissors
 */
 
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
 
+// variables
+let score = 0
+let randomNumber: number = 0
+
+// on shake
+input.onGesture(Gesture.Shake, function() {
+    
+    randomNumber = randint(0, 2)
+    basic.clearScreen()
+
+    // rock
+    if (randomNumber == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+    }
+
+    // paper
+    if (randomNumber == 1) {
+        basic.showIcon(IconNames.Square)
+    }
+
+    // scissors
+    if (randomNumber == 2) {
+        basic.showIcon(IconNames.Scissors)
+    }
+
+    // reset
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+
+})
+
+// score increase
+input.onButtonPressed(Button.A, function() {
+    
+    score += 1
+    basic.showIcon(IconNames.Yes)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)
+
+})
+
+// shows score
+input.onButtonPressed(Button.B, function()  {
+
+    basic.clearScreen()
+    basic.showString("Score:" + score)
+
+})
